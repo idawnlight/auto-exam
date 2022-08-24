6750dbd84d1a17939c1e89f819cafe0c1cb4173a
idawnlight <idawn@live.com>
2022-08-24 15:19:31 +0800

    final: real

---
+++ README.md
@@ -0,0 +1,5 @@
+# auto-exam
+
+某福建三本的暑期学校 Qt *大*作业。因为种种意外它最后在 macOS、Windows 和 openSUSE 三个平台上跑了一遍。
+
+Last commit from openSUSE.
--- src/main.cpp
+++ src/main.cpp
@@ -15,6 +15,7 @@
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
+
     EntryWidget entry;
     EditWindow editWindow;
     ExamWindow examWindow;
